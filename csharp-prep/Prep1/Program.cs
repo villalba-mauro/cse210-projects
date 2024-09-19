428da940af9c8ead00b0a748ba4285ae36836a37
villalba-mauro <vil23014@byui.edu>
2024-09-19 17:44:47 -0300

    prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string Fname = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+         string Lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Fname}, {Fname} {Lname}.");
     }
 }
