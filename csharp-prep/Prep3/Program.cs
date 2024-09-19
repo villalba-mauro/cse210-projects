e3c8558677340de1407872a331a98f84a6333eb4
villalba-mauro <vil23014@byui.edu>
2024-09-19 20:32:47 -0300

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int user_guess = 0;
+        int magic_number;
+
+        Console.WriteLine("What is the magic number?");
+        string input = Console.ReadLine();
+        magic_number = int.Parse(input);
+
+        while (user_guess != magic_number)
+        {
+        Console.WriteLine("Whats is your guess?");
+        string guess = Console.ReadLine();
+        user_guess = int.Parse(guess);
+
+            if(user_guess == magic_number )
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            else if (user_guess > magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+        
     }
 }
