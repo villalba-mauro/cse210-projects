ec894a473b287fdd24dbc1e3ceb7dc46df6f99c0
villalba-mauro <vil23014@byui.edu>
2024-09-19 19:35:09 -0300

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string input = Console.ReadLine();
+
+        int percentage = int.Parse(input);
+
+        string grade;
+
+        if (percentage >=90)
+        {
+            grade = "A";
+            Console.WriteLine("Congratulations on your hard work and effort, you have passed the course.");
+        }
+
+        else if (percentage >=80)
+        {
+            grade = "B";
+            Console.WriteLine("Congratulations, you have passed the course.");
+        }
+
+         else if (percentage >= 70)
+        {
+            grade = "C";
+            Console.WriteLine("well done you have passed the course, keep up the good work.");
+        }
+
+        else if (percentage >= 60)
+        {
+            grade = "D";
+            Console.WriteLine("You have not passed the course, keep trying hard for the next one.");
+        }
+
+        else
+        {
+            grade = "F";
+            Console.WriteLine("I'm sorry you haven't passed the course, you need to review the content given again.");
+        }
+
+        bool pass_class = percentage >= 70;
     }
 }
