8b92a6c286460ca7d78c387cc11a6e9f05308dfd
villalba-mauro <vil23014@byui.edu>
2024-09-19 20:39:00 -0300

    p3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,8 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Random Generator = new Random();
         int user_guess = 0;
-        int magic_number;
+        int magic_number = Generator.Next(1,100);
 
         Console.WriteLine("What is the magic number?");
         string input = Console.ReadLine();
