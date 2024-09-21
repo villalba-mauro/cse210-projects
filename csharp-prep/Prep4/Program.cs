585a5aef785bf6899349ff6bf0867758a1010fff
villalba-mauro <vil23014@byui.edu>
2024-09-21 09:49:23 -0300

    prep5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       
+        List<int> numbers = new List<int>();
+        int input =-1;
+
+        while (input != 0)
+        {
+            Console.WriteLine("Enter a number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        
+            int sum = 0;
+            int largest = numbers[0];
+
+            foreach(int number in numbers)
+            {
+                sum += number;
+
+                if (number > largest)
+                {
+                    largest = number;
+                }
+            }
+
+            float average = ((float)sum) / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The largest number is: {largest}");
+            
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+
+        string name = PromptUserName();
+        double number = PromptUserNumber();
+        double result = Math.Sqrt(number);
+
+        DisplayResult(name, result);
     }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name:");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static double PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favourite number:");
+        double input = double.Parse(Console.ReadLine());
+        return input;
+        
+    }
+
+    static void DisplayResult(string name, double result )
+    {
+        Console.WriteLine($"{name}, the square of your number is {result}.");
+    }
+    
 }
